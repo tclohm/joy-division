48a5bd7e194f8f2e7d12d2956fcf547e436e66a2
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-08-14 13:08:24 -0700

    working through canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  let canvasRef = useRef(null)
+  let context;
+  let size = window.innerWidth;
+  let dpr = window.devicePixelRatio;
+  let lines = [];
+
+  function createLines() {
+
+
+    let step = 30;
+
+    for (let i = step; i <= size - step; i += step) {
+      var line = [];
+      for (let j = step; j <= size - step; j += step) {
+        let distanceToCenter = Math.abs((400+30) / 2)
+        let variance = Math.max(size / 2 - 50 - distanceToCenter, 0)
+        let random = Math.random() * variance / 2 * -1
+        let point = { x: j, y: i + random }
+        line.push(point)
+      }
+      lines.push(line)
+    }
+  }
+
+  function createArt() {
+    let i;
+    let j;
+    context = canvasRef.current.getContext('2d')
+
+    canvasRef.current.width = size;
+    canvasRef.current.height = size * dpr;
+    context.scale(dpr, dpr);
+    context.lineWidth = 2;
+    
+    for (i = 5; i <= lines.length; i++) { 
+      context.beginPath()
+      context.moveTo(lines[5][0].x, lines[5][0].y)
+
+      for (j = 0; j < lines[5].length - 2; j++) {
+        let xc = (lines[5][j].x + lines[5][j + 1].x) / 2;
+        let yc = (lines[5][j].y + lines[5][j + 1].y) / 2;
+        context.quadraticCurveTo(lines[5][j].x, lines[5][j].y, xc, yc);
+      }
+
+      context.quadraticCurveTo(lines[5][j].x, lines[5][j].y, lines[5][j + 1].x, lines[5][j + 1].y);
+      context.save()
+      context.globalCompositionOperation = 'destination-out'
+      context.restore()
+      context.stroke()
+    }
+  }
+
+  useEffect(() => {
+    createLines()
+
+    setTimeout(function() {
+      createArt()
+    }, 4000);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas style={{ 'margin': '0 auto' }} ref={canvasRef} />
   );
 }
 
